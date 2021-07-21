4ae0ba170725d40c6eeefa59fa1c098cfd395ae5
somestudentcoder <christopher.oser@student.tugraz.at>
2021-07-21 10:21:25 +0200

    removed riot

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -3,8 +3,6 @@
 import { Model } from './model';
 import { View } from './view';
 import { Controller } from './controller';
-import {component} from "riot";
-import App from './basicView.riot'
 
 declare global {
    var model: Model;
@@ -12,8 +10,6 @@ declare global {
    var controller: Controller;
 }
 
-component(App)(document.getElementById("header") || document.body,
-    { title: 'Interactive Voronoi Treemap' });
 
 window.model = new Model();
 window.view = new View();
--- webpack.config.js
+++ webpack.config.js
@@ -22,18 +22,6 @@ const config = {
                 loader: 'ts-loader',
                 exclude: /node_modules/,
             },
-            {
-                test: /\.riot$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: '@riotjs/webpack-loader',
-                    options: {
-                        hot: false, // set it to true if you are using hmr
-                        // add here all the other @riotjs/compiler options riot.js.org/compiler
-                        // template: 'pug' for example
-                    }
-                }]
-            }
         ]
     },
     plugins: [
